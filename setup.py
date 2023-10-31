c81087d0298b2abc179d8e0d1afeef7b26a24408
thef00l <thef00l@f00lsgold.com>
2023-10-31 14:37:59 -0700

    A subdomainizer for a modern era

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='subdomainizer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'termcolor==1.1.0',
+        'argparse==1.4.0',
+        'beautifulsoup4==4.6.3',
+        'requests==2.21.0',
+        'htmlmin==0.1.12',
+        'tldextract==2.2.0',
+        'colorama==0.4.4',
+        'cffi'
+    ],
+    entry_points={
+        'console_scripts': [
+            'subdomainizer = SubDomainizer:main',
+        ],
+    },
+)
